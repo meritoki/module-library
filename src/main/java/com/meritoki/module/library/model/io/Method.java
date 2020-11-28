5f3912b03048ec548e1771034c9b4ba25ddd9460
Joaquin Osvaldo Rodriguez <admin@meritoki.com>
2020-11-28 18:24:48 -0300

    Adding State and IO classes

---
+++ src/main/java/com/meritoki/module/library/model/io/Method.java
@@ -0,0 +1,8 @@
+package com.meritoki.module.library.model.io;
+
+public enum Method {
+	GET,
+	POST,
+	PUT,
+	DELETE
+}
---
+++ src/main/java/com/meritoki/module/library/model/io/Request.java
@@ -0,0 +1,37 @@
+package com.meritoki.module.library.model.io;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+public class Request {
+	@JsonProperty
+	public Method method;
+	@JsonProperty
+	public String key;//key can do a lot of things when sent to server, such as tell server how to Parse the value
+	@JsonProperty
+	public String value;//value can hold a JSON object;
+	@JsonProperty
+	public String uuid;
+	
+    public Request() {
+        this.uuid = UUID.randomUUID().toString();
+    }
+
+    @JsonIgnore
+    @Override
+    public String toString() {
+        String string = "";
+        ObjectWriter ow = new ObjectMapper().writer();//.withDefaultPrettyPrinter();
+        try {
+            string = ow.writeValueAsString(this);
+        } catch (IOException ex) {
+            System.err.println("IOException " + ex.getMessage());
+        }
+        return string;
+    }
+}
